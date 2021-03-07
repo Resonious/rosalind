cde67ec624a0bc3c8c305a06be1482034a5a2d03
Nigel Baillie <nigelbaillie@hey.com>
2021-03-07 16:24:32 +0900

    LMAO got stdin parsing working like magic

--- gc.hs
+++ gc.hs
@@ -1,5 +1,38 @@
-gcContent str = 50
+import Data.Char
+
+gcContent :: ([Char]) -> Float
+gcContent str =
+  (sum [if c == 'C' || c == 'G' then 1 else 0 | c <- str]
+  ) /
+  (fromIntegral $ length str)
+
+-- main = do
+--   print $ gcContent "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC\
+--                     \TGGGAACCTGCGGGCAGTAGGTGGAAT"
+
+startsWith :: (Eq a) => a -> [a] -> Bool
+startsWith c (x:_) = (x == c)
+
+
+insertNewEntry :: [(String, String)] -> String -> [(String, String)]
+insertNewEntry entries (_:name) = (name, ""):entries
+
+
+updateLastEntry :: [(String, String)] -> String -> [(String, String)]
+updateLastEntry (e:entries) line =
+  ((fst e), (snd e) ++ line) : entries
+
+
+parseLine :: [(String, String)] -> String -> [(String, String)]
+parseLine entries line
+  | (startsWith '>' line) = insertNewEntry line entries
+  | otherwise             = updateLastEntry line entries
+
+
+parseLines input =
+  foldl (parseLine) [] (lines input)
+
 
 main = do
-  print (gcContent "CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC\
-                   \TGGGAACCTGCGGGCAGTAGGTGGAAT")
+  contents <- getContents
+  print $ parseLines contents
